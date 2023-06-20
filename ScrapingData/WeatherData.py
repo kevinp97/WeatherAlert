49442d02e83cf04550715eba607a6cb458261156
Kevin <kevinparragalarza@gmail.com>
2023-06-20 17:36:21 +0200

    Creada una clase para sacar los datos de temperatura y precipitacion

--- ScrapingData/WeatherData.py
+++ ScrapingData/WeatherData.py
@@ -19,12 +19,51 @@ logging.basicConfig(level=logging.DEBUG)
 query = COORDINATE
 api_key = API_KEY_WAPI
 weather_url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={query}&days=1&aqi=yes&alerts=no"
-
-
+response = requests.get(weather_url).json()["forecast"]
+class Scraping():
+    
+    def __init__(self, data):
+        self.data = data
+        
+        
+    def date(self, ind):
+        # '2023-06-20 00:00' este es el formato de la fecha
+        return self.data['forecastday'][0]['hour'][ind]['time'].split(' ')[0]
+    
+    def hour(self, ind):
+        return self.data['forecastday'][0]['hour'][ind]['time'].split(' ')[1]
+        
+    def temp(self, ind):
+        return self.data['forecastday'][0]['hour'][ind]['temp_c']
+        
+    def condition(self, ind):
+        return self.data['forecastday'][0]['hour'][ind]['condition'][0]['text']
+        
+    def wind(self, ind):
+        return self.data['forecastday'][0]['hour'][ind]['wind_kph']
+        
+    def sensation(self, ind):
+        return self.data['forecastday'][0]['hour'][ind]['feelslike_c']
+        
+    def rain(self, ind):
+        return self.data['forecastday'][0]['hour'][ind]['will_it_rain']
+        
+    def prob_rain(self, ind):
+        return self.data['forecastday'][0]['hour'][ind]['chance_of_rain']
+      
+        
 if __name__ == "__main__":
-    response = requests.get(weather_url).json()
-    #Comprobamos que esta la clave forecast
-    logging.info(response.keys())
-    weather_data = response["forecast"]
-    with open('WeatherData.json', 'w') as jf: 
-        json.dump(weather_data, jf, ensure_ascii=False, indent=4)
+    # response = requests.get(weather_url).json()
+    # #Comprobamos que esta la clave forecast
+    # logging.info(response.keys())
+    # weather_data = response["forecast"]
+    # with open('WeatherData.json', 'w') as jf: 
+    #     json.dump(weather_data, jf, ensure_ascii=False, indent=4)
+        
+    # logging.warning(weather_data['forecastday'][0]['hour'][23]['time'])
+        
+    #Obtenemos la fecha
+    datos = Scraping(response)
+    for i in range(24):
+        fecha = datos.hour(i)
+        logging.info(fecha)
